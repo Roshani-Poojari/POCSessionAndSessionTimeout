df2ab7adb11cacf7c33847226e5be26fe7282bc8
Roshani-Poojari <roshanipoojari2002@gmail.com>
2024-09-05 20:20:27 +0530

    first commit

---
+++ POCSession/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace POCSession.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            if (Session["Username"] != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            // Simulate user validation
+            if (username == "testuser" && password == "password")
+            {
+                Session["Username"] = username;
+                Session["LoggedInTime"] = DateTime.Now;
+                return RedirectToAction("Index", "Home");
+            }
+
+            // If login fails, remain on the login page
+            ViewBag.Error = "Invalid username or password";
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear(); // Clears all session data
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult SessionTimeout()
+        {
+            return View();
+        }
+    }
+}
---
+++ POCSession/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace POCSession.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["Username"] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            // Check if session has timed out
+            DateTime? loggedInTime = Session["LoggedInTime"] as DateTime?;
+            if (loggedInTime != null && DateTime.Now > loggedInTime.Value.AddMinutes(1))
+            {
+                Session.Clear();
+                return RedirectToAction("SessionTimeout", "Account");
+            }
+
+            return View();
+        }
+    }
+}
